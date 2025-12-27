0b04e81a0e08cab84698816d3f49931ebd3e69bb
x3nonxe <rohmanahmadtaufiq@gmail.com>
2025-12-27 17:01:57 +0700

    refactor: remove unused files and implement main application structure
    - Deleted obsolete router and error handling files.
    - Introduced main.go to serve as the entry point for the application.
    - Added configuration loading functionality in utils/config.go.
    - Implemented currency utility functions in utils/currency.go.
    - Added password hashing and checking utilities in utils/password.go.
    - Created random data generation utilities in utils/random.go.
    - Updated sqlc configuration for database queries and migrations.
    - Added tests for password utilities in utils/password_test.go.

---
+++ api/server.go
@@ -0,0 +1,58 @@
+package api
+
+import (
+	db "github.com/X3nonxe/simplebank/db/sqlc"
+	"github.com/X3nonxe/simplebank/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+)
+
+// Server serves HTTP requests for our banking service.
+type Server struct {
+	config     utils.Config
+	store      db.Store
+	// tokenMaker token.Maker
+	router     *gin.Engine
+}
+
+// NewServer creates a new HTTP server and set up routing.
+func NewServer(config utils.Config, store db.Store) (*Server, error) {
+	server := &Server{
+		config:     config,
+		store:      store,
+	}
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
+	// router.POST("/users", server.createUser)
+	// router.POST("/users/login", server.loginUser)
+	// router.POST("/tokens/renew_access", server.renewAccessToken)
+
+	// authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
+
+	// router.POST("/transfers", server.createTransfer)
+
+	server.router = router
+}
+
+// Start runs the HTTP server on a specific address.
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
